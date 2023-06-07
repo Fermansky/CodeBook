db1b7baf99cef03cd17c11bc003148183258b571
felix-hua <2657226735@qq.com>
2023-06-08 00:02:16 +0800

    clean up

--- src/main/java/com/felixhua/codebook/ui/MainPane.java
+++ src/main/java/com/felixhua/codebook/ui/MainPane.java
@@ -7,9 +7,9 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 
 public class MainPane extends BorderPane {
     private static final MainPane MAIN_PANE = new MainPane();
